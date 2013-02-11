c1bc962736896f0b4f10f6f961bd7687ceec3260
Ryan J. Geyer <me@ryangeyer.com>
2013-02-11 08:47:04 -0800

    gem is not the same as gem_package.. Oops

--- cookbooks/rs_user_policy/recipes/install.rb
+++ cookbooks/rs_user_policy/recipes/install.rb
@@ -21,7 +21,7 @@ rightscale_marker :begin
 # Cover our bases by installing it in the Chef/RightScale sandbox, as
 # well as for the system
 chef_gem "rs_user_policy"
-gem "rs_user_policy"
+gem_package "rs_user_policy"
 
 directory node["rs_user_policy"]["home"] do
   recursive true
